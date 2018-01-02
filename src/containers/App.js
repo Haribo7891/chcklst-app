69aaa2fb3f0e26ed081c69764453428dc11c9d21
Haribo7891 <harry.crank@me.com>
2018-01-02 11:30:41 +0000

    [AddItem] static component created

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,8 +4,20 @@ import '../styles/App.css';
 class App extends Component {
   render () {
     return (
-      <div>
-        Hello world!
+      <div className="container">
+        <h1>Checklist App</h1>
+        <div className="form-group">
+          <label className="col-form-label col-form-label-lg" htmlFor="inputLarge">Add an item to your checklist:</label>
+          <form className="form-inline my-2 my-lg-0">
+            <input 
+              className="form-control mr-sm-2" 
+              type="text" 
+              placeholder="Add item..." 
+              ref="addNewItem" 
+            ></input>
+            <button className="btn btn-primary my-2 my-sm-0" type="submit" >Add</button>
+          </form>
+        </div>
       </div>
     );
   }
