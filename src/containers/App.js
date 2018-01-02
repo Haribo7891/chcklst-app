681d7e30cbfad96249f2c6c4a65f3cf8c970e2f5
Haribo7891 <harry.crank@me.com>
2018-01-02 11:38:03 +0000

    [ListCard] & [ListItem] static components created

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,9 +14,67 @@ class App extends Component {
               type="text" 
               placeholder="Add item..." 
               ref="addNewItem" 
-            ></input>
-            <button className="btn btn-primary my-2 my-sm-0" type="submit" >Add</button>
+              onChange=""></input>
+            <button className="btn btn-primary my-2 my-sm-0" type="submit" onClick="">Add</button>
           </form>
+        </div>
+        <div className="card border-success mb-3">
+          <div className="card-header">
+            <div className="header">Checklist Items ( count of Items )</div>
+          </div>
+          <div className="card-body text-success">
+            <ul className="list-group" key="">
+              <div className="items">
+                <li className="list-group-item d-flex justify-content-between align-items-center">
+                  <div className="form-check">
+                    <label className="form-check-label">
+                      <input className="form-check-input" type="checkbox" value="" checked="" onChange=""/> Item 1
+                    </label>
+                  </div>
+                  <button type="button" className="btn btn-outline-danger btn-sm" onClick="">Remove</button>
+                </li>
+              </div>
+              <div className="items">
+                <li className="list-group-item d-flex justify-content-between align-items-center">
+                  <div className="form-check">
+                    <label className="form-check-label">
+                      <input className="form-check-input" type="checkbox" value="" checked="" onChange=""/> Item 2
+                    </label>
+                  </div>
+                  <button type="button" className="btn btn-outline-danger btn-sm" onClick="">Remove</button>
+                </li>
+              </div>
+            </ul>
+          </div>
+        </div>
+        <div className="card border-success mb-3">
+          <div className="card-header">
+            <div className="header">Checklist Items ( count of Items )</div>
+          </div>
+          <div className="card-body text-success">
+            <ul className="list-group" key="">
+              <div className="items">
+                <li className="list-group-item d-flex justify-content-between align-items-center">
+                  <div className="form-check">
+                    <label className="form-check-label">
+                      <input className="form-check-input" type="checkbox" value="" checked="" onChange=""/> Item 1
+                    </label>
+                  </div>
+                  <button type="button" className="btn btn-outline-danger btn-sm" onClick="">Remove</button>
+                </li>
+              </div>
+              <div className="items">
+                <li className="list-group-item d-flex justify-content-between align-items-center">
+                  <div className="form-check">
+                    <label className="form-check-label">
+                      <input className="form-check-input" type="checkbox" value="" checked="" onChange=""/> Item 2
+                    </label>
+                  </div>
+                  <button type="button" className="btn btn-outline-danger btn-sm" onClick="">Remove</button>
+                </li>
+              </div>
+            </ul>
+          </div>
         </div>
       </div>
     );
