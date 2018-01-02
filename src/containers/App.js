d073a0327d5712746f4b11c8c3840365cde961ab
Haribo7891 <harry.crank@me.com>
2018-01-02 11:40:52 +0000

    [UncheckItems] & [DeleteItems] static components created

--- src/containers/App.js
+++ src/containers/App.js
@@ -76,6 +76,14 @@ class App extends Component {
             </ul>
           </div>
         </div>
+        <div className="buttons justify-content-between align-items-center">
+          <span className="">
+            <button type="button" className="btn btn-primary btn-lg" onClick="">Uncheck all items</button>
+          </span>
+          <span className="">
+            <button type="button" className="btn btn-danger btn-lg" onClick="">Delete all items</button>
+          </span>
+        </div>
       </div>
     );
   }
